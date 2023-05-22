91f4515e42be5dae10393d776c32486ca317e19e
mcarricerutti <martinacarri345@gmail.com>
2023-05-22 19:43:02 -0300

    actualizacion

--- DesafioPracticaIntegradora/src/routes/cart.routes.js
+++ DesafioPracticaIntegradora/src/routes/cart.routes.js
@@ -1,48 +1,33 @@
 import {Router} from 'express';
-// import {CartManager} from '../CartManager.js';
 import { cartModel } from '../models/Cart.js';
-
-// const cartManager = new CartManager('./src/carts.txt','./src/products.txt');
-const cartRouter = Router(); //Router para manejo de rutas
-
-//FS -------------------------------------------------------------------------------------------
-
-// cartRouter.get('/:cid', async (req, res) => {
-//     const cid= req.params.cid
-//     const cart=await cartManager.getCartById(cid);//obtenemos los productos
-//     res.send(cart.products??cart);//enviamos los productos
-// });
-
-// cartRouter.post("/:cid/product/:pid", async (req, res) => {
-//     const cid= req.params.cid;
-//     const pid= req.params.pid;
-//     const { quantity } = req.body //Consulto el dato quantity enviado por postman
-//     res.send(await cartManager.addProduct(cid,pid,quantity))
-// })
+const cartRouter = Router();
 
 
-//MONGO -------------------------------------------------------------------------------------------
+cartRouter.post('/', async (req, res) => {
+    try {
+        const cart = await cartModel.create([{products:[]}])
+        res.send(cart)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
 cartRouter.get('/:cid', async (req, res) => {
     const cid= req.params.cid
     try {
-        const cart=await cartModel.findById(cid);//obtenemos los productos
+        const cart=await cartModel.findById(cid);
         res.send(cart);
     } catch (error) {
         res.send(error);
     }
-    //enviamos los productos
 });
 
 cartRouter.post("/:cid/product/:pid", async (req, res) => {
     const cid= req.params.cid;
     const pid= req.params.pid;
-    const { quantity } = req.body //Consulto el dato quantity enviado por postman
-    try {
-        const updatedCart=await cartModel.findByIdAndUpdate(cid,{$push:{products:{id_prod:pid,quantity:quantity}}},{new:true});
-        res.send(updatedCart);
-    } catch (error) {
-        res.send(error)
-    }
+    const { quantity } = req.body
+    const addProduct = await cartModel.insertMany([{cid, products: [{id_prod: pid, quantity: quantity}]}])
+    res.send(addProduct)
 })
 
 export default cartRouter;
