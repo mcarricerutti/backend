830a5d26c656016adb9fe078103aa8faed184aae
mcarricerutti <martinacarri345@gmail.com>
2023-06-05 10:25:44 -0300

    cambio

--- Desafio7SegundaPreentrega/src/routes/views.router.js
+++ Desafio7SegundaPreentrega/src/routes/views.router.js
@@ -10,24 +10,24 @@ router.get("/chat", (req, res) => {
   res.render("socket");
 });
 
-router.get("/", async (req, res) => {
-  await manager
-    .getProducts()
-    .then((document) => {
-      const context = {
-        usersDocuments: document.map((doc) => {
-          return {
-            title: doc.title,
-            description: doc.description,
-          };
-        }),
-      };
-      res.render("home", {
-        usersDocuments: context.usersDocuments,
-      });
-    })
-    .catch((error) => res.status(500).send(error));
-});
+// router.get("/", async (req, res) => {
+//   await manager
+//     .getProducts()
+//     .then((document) => {
+//       const context = {
+//         usersDocuments: document.map((doc) => {
+//           return {
+//             title: doc.title,
+//             description: doc.description,
+//           };
+//         }),
+//       };
+//       res.render("home", {
+//         usersDocuments: context.usersDocuments,
+//       });
+//     })
+//     .catch((error) => res.status(500).send(error));
+// });
 
 router.get("/agregar", (req, res) => {
   res.render("realTimeProducts");
